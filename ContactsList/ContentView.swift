f499c7c0be0f6a64fc9d4d7a19bfcb46e828149f
AnnGolub <anka.golub17@gmail.com>
2021-06-16 11:05:40 +0300

    Initial Commit

---
+++ ContactsList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ContactsList
+//
+//  Created by Анна Голубева on 16.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
